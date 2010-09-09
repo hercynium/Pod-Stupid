e1ded9b30ac5832a629edf068e14279422d48d21
Stephen R. Scaffidi <stephen@scaffidi.net>
2010-09-08 21:40:36 -0400

    test parse & reassemble against Pod::Simple's corpus

--- test_pod_parser.pl
+++ test_pod_parser.pl
@@ -3,24 +3,28 @@
 use strict;
 use warnings;
 
+use Test::More;
+
 use File::Slurp qw( read_file );
 use Data::Dumper qw( Dumper );
 use Pod::Simplest qw( parse_string );
 
 # for each file, parse out the pod, then insert it back into the 
 # stripped version and assert they're the same.
-for my $file ( @ARGV ) {
+for my $file ( glob "t/corpus{,2}/*" ) {
+
     my $text = read_file( $file );
+
     my ( $pieces, $stripped_text ) = parse_string( $text );
 
-    print Dumper $pieces;
+#    print Dumper $pieces;
 #    print $stripped_text;
 
     # put the pod back in the stripped text, just to test...
     substr( $stripped_text, $_->{start_pos}, 0, $_->{orig_txt} ) 
         for grep { ! exists $_->{non_pod} } @$pieces;
 
-    print $stripped_text eq $text ? "ok - $file\n" : "not ok - $file\n";
+    ok $stripped_text eq $text, $file;
 }
 
-
+done_testing();
